d454c75dc704851032453ddcd7b589b8b45b05a9
Azeem <livelyazeem@gmail.com>
2023-01-24 08:37:59 +0500

    learning pern

--- server/db.js
+++ server/db.js
@@ -1,2 +1,12 @@
-const Pool = require('pg').Pool
+const Pool = require("pg").Pool;
+require(process.env());
 
+const pool = new Pool({
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  host: "localhost",
+  port: 5432,
+  database: "perntodo",
+});
+
+module.exports = pool;
