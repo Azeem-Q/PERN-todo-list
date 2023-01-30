4fe9abe294e38762b9811ff9988649a332cc4d33
Azeem <livelyazeem@gmail.com>
2023-01-30 08:47:03 +0500

    learning pern

--- client/src/components/EditTodo.js
+++ client/src/components/EditTodo.js
@@ -2,20 +2,40 @@ import React, { Fragment, useState } from "react";
 
 const EditTodo = ({ todo }) => {
   const [description, setDescription] = useState(todo.description);
+
+  // edit description function
+
+  const updateDescription = async (e) => {
+    try {
+      const body = { description };
+      const response = await fetch(
+        `http://localhost:5000/todos/${todo.todo_id}`,
+        {
+          method: "PUT",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(body),
+        }
+      );
+      window.location = "/";
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
   return (
     <Fragment>
       <button
         type="button"
         className="btn btn-warning"
         data-bs-toggle="modal"
-        data-bs-target="#myModal"
+        data-bs-target={`#id${todo.todo_id}`}
       >
         Edit
       </button>
 
       <div
         className="modal"
-        id="myModal"
+        id={`id${todo.todo_id}`}
+        onClick={() => setDescription(todo.description)}
       >
         <div className="modal-dialog">
           <div className="modal-content">
@@ -25,6 +45,7 @@ const EditTodo = ({ todo }) => {
                 type="button"
                 className="btn-close"
                 data-bs-dismiss="modal"
+                onClick={() => setDescription(todo.description)}
               ></button>
             </div>
 
@@ -42,6 +63,7 @@ const EditTodo = ({ todo }) => {
                 type="button"
                 className="btn btn-warning"
                 data-bs-dismiss="modal"
+                onClick={(e) => updateDescription(e)}
               >
                 Edit
               </button>
@@ -49,6 +71,7 @@ const EditTodo = ({ todo }) => {
                 type="button"
                 className="btn btn-danger"
                 data-bs-dismiss="modal"
+                onClick={() => setDescription(todo.description)}
               >
                 Close
               </button>
